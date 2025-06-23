26f17d3d95e0b4359dec5e24cc0ddb42c865347f
bulkswap <info@easyclickmedia.com>
2025-06-23 23:49:16 +0200

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,14 +1,22 @@
-
+// pages/api/generate.js
 import { nanoid } from 'nanoid';
-import redis from '../../lib/redis';
+import { Redis } from '@upstash/redis';
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 export default async function handler(req, res) {
-  const slug = nanoid(10);
-  const key = `redirect:${slug}`;
-  const value = JSON.stringify({
+  let slug;
+  do {
+    slug = nanoid(10);
+  } while (await redis.get(`slug-${slug}`));
+
+  await redis.set(`slug-${slug}`, {
     target: "https://tikkie.me/pay/v5e4jnd2iqbhe7o53k0n",
-    createdAt: Date.now()
+    firstClick: null,
   });
-  await redis.set(key, value);
+
   res.status(200).json({ slug });
 }
