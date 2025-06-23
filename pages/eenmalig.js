b682ddaa2c01a8875b8b17ed9e1c5927ce8913f6
bulkswap <info@easyclickmedia.com>
2025-06-23 16:08:44 +0200

    Add files via upload

---
+++ pages/eenmalig.js
@@ -0,0 +1,8 @@
+export default function Eenmalig() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Eenmalige link</h1>
+      <p>Deze link is eenmalig te gebruiken binnen 60 minuten.</p>
+    </div>
+  );
+}
---
+++ pages/expired.js
@@ -0,0 +1,8 @@
+export default function Expired() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Link verlopen</h1>
+      <p>Deze link is niet meer geldig.</p>
+    </div>
+  );
+}
