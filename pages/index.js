47d80a0f8eac7702fd702717a25dde91636e7a61
bulkswap <info@easyclickmedia.com>
2025-06-24 02:43:28 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: 'https://www.abnamro.nl/nl/prive/internet-en-mobiel/apps/tikkie/index.html',
+      permanent: false,
+    },
+  };
+}
 
 export default function Home() {
-  return <h1>Welkom bij Tijdlink</h1>;
+  return null;
 }
