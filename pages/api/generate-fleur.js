201cdc5e04b539073b0b7487b2c5c4ee7b5d1550
bulkswap <info@easyclickmedia.com>
2025-08-03 00:00:31 +0200

    Update generate-fleur.js

--- pages/api/generate-fleur.js
+++ pages/api/generate-fleur.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   } while (await redis.get(`slug-${slug}`));
 
   await redis.set(`slug-${slug}`, {
-    target: "https://tikkie.me/pay/p98fat5qns9bh2aaafv1",
+    target: "https://tikkie.me/pay/hrrlc7elol7jcmatmuq1",
     firstClick: null,
   });
 
