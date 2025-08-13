c25798919f06107eb81cec399d6405ca86aadb69
bulkswap <info@easyclickmedia.com>
2025-08-13 19:35:08 +0200

    Update nicole.js

--- pages/nicole.js
+++ pages/nicole.js
@@ -30,7 +30,7 @@ export default function Nicole() {
     `💖 Wil je mij alsjeblieft betalen voor 'Tikkie' via ${baseUrl}/${slug}\n\nVia deze link kun je €${amount} betalen`;
 
   const generateBolText = (amount, url) =>
-    `💖 Wil je mij alsjeblieft betalen via bol cadeaukaart? Binnen 1 minuut via: ${url}\n\nVia deze link kun je €${amount} betalen`;
+    `💖 Wil je mij alsjeblieft betalen via bol cadeaukaart? Binnen 1 minuut via: ${url}\n\nVia deze link kun je €${amount} betalen\n\nVul mijn emailadres in: colindabrand@outlook.com`;
 
   const bankText = `Liever via handmatige bankoverschrijving betalen? Dat kan ook: NL34BUNQ2106132808 tnv K. Bohak`;
 
