8518687826c962142548634b8360883b54fdd433
bulkswap <info@easyclickmedia.com>
2025-08-13 19:32:42 +0200

    Add files via upload

---
+++ pages/api/generate-tikkie-2.js
@@ -0,0 +1,22 @@
+// pages/api/generate.js
+import { nanoid } from 'nanoid';
+import { Redis } from '@upstash/redis';
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export default async function handler(req, res) {
+  let slug;
+  do {
+    slug = nanoid(13);
+  } while (await redis.get(`slug-${slug}`));
+
+  await redis.set(`slug-${slug}`, {
+    target: " https://tikkie.me/pay/f1hbj30ebc9qmj1jt74d",
+    firstClick: null,
+  });
+
+  res.status(200).json({ slug: `pay/${slug}` });
+}
