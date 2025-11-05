df514c00c545c44a7f0e2ecaee7c7f022012e1b0
bulkswap <info@easyclickmedia.com>
2025-11-05 20:27:14 +0100

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   } while (await redis.get(`slug-${slug}`));
 
   await redis.set(`slug-${slug}`, {
-    target: "https://tikkie.me/pay/l2sqgdamqq486varlbpu",
+    target: "https://tikkie.me/pay/fre7c3fn1e7i8a1r8ldu",
     firstClick: null,
   });
 
