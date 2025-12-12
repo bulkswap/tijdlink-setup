edbe7c4fc1c4ebb0a8887796c6b3648a1b251651
bulkswap <info@easyclickmedia.com>
2025-12-12 02:27:29 +0100

    Update generate-nicole-2.js

--- pages/api/generate-nicole-2.js
+++ pages/api/generate-nicole-2.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   } while (await redis.get(`slug-${slug}`));
 
   await redis.set(`slug-${slug}`, {
-    target: " https://tikkie.me/pay/s8k93p6gq9pf3dpbm1ob",
+    target: " https://tikkie.me/pay/1frpk5e04i55qb2ocvtt",
     firstClick: null,
   });
 
