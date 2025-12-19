98f65560ddbd4e56f53cc286ad1cdd5296a94c9d
lc76701 <info@easyclickmedia.com>
2025-12-19 17:50:24 +0100

    Update [slug].js

--- pages/verify/[slug].js
+++ pages/verify/[slug].js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'; 
 
 export async function getServerSideProps({ params }) {
   return { props: { slug: params.slug } };
