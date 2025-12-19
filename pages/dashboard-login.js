5f65340d56a303737cf2a4b8f18a08f003d91d37
lc76701 <info@easyclickmedia.com>
2025-12-19 09:49:55 +0100

    Create dashboard-login.js

---
+++ pages/dashboard-login.js
@@ -0,0 +1,17 @@
+export default function DashboardLogin() {
+  const login = () => {
+    const pw = prompt('Wachtwoord dashboard');
+    if (pw === '2026') {
+      window.location.href = '/dashboard?p=2026';
+    } else {
+      alert('Onjuist wachtwoord');
+    }
+  };
+
+  return (
+    <div style={{ padding: '3rem', fontFamily: 'sans-serif' }}>
+      <h1>Dashboard</h1>
+      <button onClick={login}>Inloggen</button>
+    </div>
+  );
+}
