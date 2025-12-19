fbd519c73decf206239e0467dd51a66835fe8aa8
lc76701 <info@easyclickmedia.com>
2025-12-19 07:13:13 +0100

    Update store-location.js

--- pages/api/store-location.js
+++ pages/api/store-location.js
@@ -1,10 +1,11 @@
 export default async function handler(req, res) {
   const { slug, lat, lng, accuracy } = req.body;
+  const userAgent = req.headers['user-agent'] || 'unknown';
 
   const redisUrl = process.env.UPSTASH_REDIS_REST_URL;
   const redisToken = process.env.UPSTASH_REDIS_REST_TOKEN;
 
-  await fetch(`${redisUrl}/set/location-${slug}-${Date.now()}`, {
+  await fetch(`${redisUrl}/set/log-${slug}-${Date.now()}`, {
     method: 'POST',
     headers: {
       Authorization: `Bearer ${redisToken}`,
@@ -15,6 +16,7 @@ export default async function handler(req, res) {
       lat,
       lng,
       accuracy,
+      userAgent,
       time: Date.now(),
     }),
   });
