2cd9e4ca42c232be1164b6438bb4f23230ad6a22
lc76701 <info@easyclickmedia.com>
2025-12-19 07:29:55 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+              "style-src 'self' 'unsafe-inline'",
+              "img-src 'self' data:",
+              "connect-src *",
+            ].join('; '),
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
