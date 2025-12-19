0138ba99dc4cc777e7174db7a324e98a77d12ca4
lc76701 <info@easyclickmedia.com>
2025-12-19 06:34:40 +0100

    Create generate-fleur-verify.js

---
+++ pages/api/generate-fleur-verify.js
@@ -0,0 +1,24 @@
+import { nanoid } from 'nanoid';
+import { Redis } from '@upstash/redis';
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export default async function handler(req, res) {
+  const { flow = 'verify' } = req.query; // verify | verify-blocked
+
+  let slug;
+  do {
+    slug = nanoid(13);
+  } while (await redis.get(`slug-${slug}`));
+
+  await redis.set(`slug-${slug}`, {
+    target: "https://tikkie.me/pay/1frpk5e04i55qb2ocvtt",
+    flow,
+    firstClick: null,
+  });
+
+  res.status(200).json({ slug });
+}
