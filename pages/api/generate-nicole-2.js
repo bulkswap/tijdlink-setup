f264f7d514b5b507f9b491c0635eaa61eec34000
lc76701 <info@easyclickmedia.com>
2026-01-14 20:24:48 +0100

    Update payment link in generate-nicole-2 API

--- pages/api/generate-nicole-2.js
+++ pages/api/generate-nicole-2.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   } while (await redis.get(`slug-${slug}`));
 
   await redis.set(`slug-${slug}`, {
-    target: "https://tikkie.me/pay/gcout436smlv8iregg3o",
+    target: "https://tikkie.me/pay/q07e26r6qvefemb5ump6",
     firstClick: null,
   });
 
